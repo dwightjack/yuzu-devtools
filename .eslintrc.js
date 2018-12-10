349aa96833078d3c31f42f4d6c71b348a8654b0c
Marco Solazzi <marco.solazzi@gmail.com>
2018-12-10 15:54:32 +0900

    allow dev deps in webpack configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,7 +18,7 @@ module.exports = {
     'import/prefer-default-export': 0,
     'import/no-extraneous-dependencies': [
       'error',
-      { devDependencies: ['**/*.test.js', 'config/*.js'] },
+      { devDependencies: ['**/*.test.js', 'config/*.js', 'webpack.*.js'] },
     ],
   },
   overrides: [
