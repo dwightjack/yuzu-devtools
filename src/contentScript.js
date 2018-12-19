09588618e79bfd137a471dfd236e7dd728e82593
Marco Solazzi <marco.solazzi@gmail.com>
2018-12-19 12:47:11 +0900

    keep variables private

--- src/contentScript.js
+++ src/contentScript.js
@@ -1,46 +1,48 @@
-const script = document.createElement('script');
+(() => {
+  const script = document.createElement('script');
 
-function bindMessage() {
-  if (window.__YUZU_DEVTOOLS_GLOBAL_HOOK__) {
-    window.__YUZU_DEVTOOLS_GLOBAL_HOOK__.on('*', (type, action, ...args) => {
-      window.postMessage(
-        {
-          source: 'yuzu-devtools',
-          payload: {
-            type,
-            action,
-            args,
+  function bindMessage() {
+    if (window.__YUZU_DEVTOOLS_GLOBAL_HOOK__) {
+      window.__YUZU_DEVTOOLS_GLOBAL_HOOK__.on('*', (type, action, ...args) => {
+        window.postMessage(
+          {
+            source: 'yuzu-devtools',
+            payload: {
+              type,
+              action,
+              args,
+            },
           },
-        },
-        '*',
-      );
-    });
-  }
-  window.__YUZU_DEVTOOLS_GLOBAL_HOOK__.start();
-}
-
-script.textContent = `(${bindMessage.toString()}());`;
-
-document.documentElement.appendChild(script);
-
-script.parentNode.removeChild(script);
-
-window.addEventListener('message', (event) => {
-  // Only accept messages from the same frame
-  if (event.source !== window) {
-    return;
+          '*',
+        );
+      });
+    }
+    window.__YUZU_DEVTOOLS_GLOBAL_HOOK__.start();
   }
 
-  const message = event.data;
+  script.textContent = `(${bindMessage.toString()}());`;
 
-  // Only accept messages that we know are ours
-  if (
-    typeof message !== 'object' ||
-    message === null ||
-    message.source !== 'yuzu-devtools'
-  ) {
-    return;
-  }
+  document.documentElement.appendChild(script);
 
-  chrome.runtime.sendMessage(message.payload);
-});
+  script.parentNode.removeChild(script);
+
+  window.addEventListener('message', (event) => {
+    // Only accept messages from the same frame
+    if (event.source !== window) {
+      return;
+    }
+
+    const message = event.data;
+
+    // Only accept messages that we know are ours
+    if (
+      typeof message !== 'object' ||
+      message === null ||
+      message.source !== 'yuzu-devtools'
+    ) {
+      return;
+    }
+
+    chrome.runtime.sendMessage(message.payload);
+  });
+})();
