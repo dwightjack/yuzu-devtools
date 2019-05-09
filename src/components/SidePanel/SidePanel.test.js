76076e6f45ddc2209c2485c70b69bdb0115c832f
Marco Solazzi <marco.solazzi@gmail.com>
2019-05-09 18:51:11 +0900

    SidePanel as a container

--- src/components/SidePanel/SidePanel.test.js
+++ src/components/SidePanel/SidePanel.test.js
@@ -1,24 +1,22 @@
 import { toHTML } from '../test-utils';
 import SidePanel from './SidePanel';
 
-const data = { uid: '_0', name: 'DEMO' };
-
 describe('SidePanel', () => {
   test('default snapshot', () => {
-    const { html } = toHTML(SidePanel(data));
-    expect(html).toMatchSnapshot();
-  });
-  test('default component title snapshot', () => {
-    const { html } = toHTML(SidePanel({ ...data, name: null }));
-    expect(html).toMatchSnapshot();
-  });
-  test('empty snapshot', () => {
     const { html } = toHTML(SidePanel());
     expect(html).toMatchSnapshot();
   });
 
-  test('has a slot', () => {
-    const wrapper = toHTML(SidePanel(data));
-    expect(wrapper.find('slot')).toEqual(expect.any(HTMLSlotElement));
+  test('has a header slot', () => {
+    const wrapper = toHTML(SidePanel());
+    expect(wrapper.find('slot[name="header"]')).toEqual(
+      expect.any(HTMLSlotElement),
+    );
+  });
+  test('has a body slot', () => {
+    const wrapper = toHTML(SidePanel());
+    expect(wrapper.find('slot[name="body"]')).toEqual(
+      expect.any(HTMLSlotElement),
+    );
   });
 });
